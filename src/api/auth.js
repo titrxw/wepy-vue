4f25b8758fd0b23a60d840492646eab0410c5200
rxw <1120309488@qq.com>
2018-06-21 18:41:00 +0800

    Add files via upload

---
+++ src/api/auth.js
@@ -0,0 +1,38 @@
+import base from './base'
+
+/**
+ * 登录类
+ */
+export default class Auth extends base {
+  static async miniProgramLogin(code) {
+    let result = await this.post('mc/common/miniProgramLogin', {
+      code: code
+    })
+    if (result) {
+      this.G.openId = result.openid;
+    }
+    return result
+  }
+  static async autoLogin() {
+    let result = await this.post('mc/common/login', {
+      openid: this.G.openId
+    })
+    if (result) {
+      this.G.workId = result.worker_id
+      this.G.token = result.token;
+    }
+    return result
+  }
+  static async register(params) {
+    let result = await this.post('mc/common/bind', {
+      form: params
+    })
+    if (result) {
+      this.G.token = result.token
+    }
+    return result
+  }
+  static unLogin() {
+    this.G.token = null
+  }
+}
---
+++ src/api/base.js
@@ -0,0 +1,42 @@
+import G from '@/conf';
+import wepy from 'wepy';
+
+export default class base {
+  static G = G
+
+  static post(url, data, header = {}) {
+    return new Promise((resolve, reject) => {
+      wepy.request({
+        url: url,
+        data: data,
+        method: 'POST',
+        header: {
+          ...header,
+          'content-type': 'application/x-www-form-urlencoded' // 默认值
+        },
+        success: function (res) {
+          resolve(res)
+        },
+        fail: function (error) {
+          reject(reject)
+        }
+      })
+    });
+  }
+  static get(url, data, header = {}) {
+    return new Promise((resolve, reject) => {
+      wepy.request({
+        url: url,
+        data: data,
+        method: 'GET',
+        header: header,
+        success: function (res) {
+          resolve(res)
+        },
+        fail: function (error) {
+          reject(reject)
+        }
+      })
+    });
+  }
+}
