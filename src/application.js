c0cdc4fff5c723d3b47e2102fa26a852107dcdd8
titrxw <1120309488@qq.com>
2018-07-02 22:42:21 +0800

    change

--- src/application.js
+++ src/application.js
@@ -7,147 +7,101 @@ import Tip from 'tip';
 import Validate from './libs/validate'
 
 export default class app extends wepy.app {
-  mounted(options) {
+    mounted(options) {
 
-  }
+    }
 
-  onLaunch(options) {
-    this.intercept('request', {
-      config(res) {
-        if (!res.data) {
-          res.data = {}
-        }
-        res.data.token = G.token;
-        res.data = qs.stringify(res.data);
+    onLaunch(options) {
+        this.intercept('request', {
+            config(res) {
+                if (!res.data) {
+                    res.data = {}
+                }
+                res.data.token = G.token;
+                res.data = qs.stringify(res.data);
 
-        if (!Validate.isUrl(res.url)) {
-          res.url = G.apiHost + res.url;
-        }
+                if (!Validate.isUrl(res.url)) {
+                    res.url = G.apiHost + res.url;
+                }
 
-        if (res.method == 'GET') {
-          res.url = res.url + '?' + res.data;
-        }
+                if (res.method == 'GET') {
+                    res.url = res.url + '?' + res.data;
+                }
 
-        return res;
-      },
-      success(res) {
-        if (typeof res.data == 'string') {
-          // 这里暂时有问题
-          Tip.errorToast('数据格式错误' + res.data);
-          return false;
-        }
-        // 业务需求
-        switch (res.data.ret) {
-          case 200:
-            return res.data.data;
-          case 401:
-            api.unLogin()
-            wx.redirectTo({
-              url: '/login'
-            });
-            return false;
-          case 302:
-            api.unLogin()
-            wx.redirectTo({
-              url: '/login'
-            });
-            return false;
-          default:
-            if (res.data.ret) {
-              Tip.errorToast(res.data.msg);
-              return false;
+                return res;
+            },
+            success(res) {
+                if (typeof res.data == 'string') {
+                    // 这里暂时有问题
+                    Tip.errorToast('数据格式错误' + res.data);
+                    return false;
+                }
+                // 业务需求
+                switch (res.data.ret) {
+                    case 200:
+                        return res.data.data;
+                    case 401:
+                        api.unLogin()
+                        wx.redirectTo({
+                            url: '/login'
+                        });
+                        return false;
+                    case 302:
+                        api.unLogin()
+                        wx.redirectTo({
+                            url: '/login'
+                        });
+                        return false;
+                    default:
+                        if (res.data.ret) {
+                            Tip.errorToast(res.data.msg);
+                            return false;
+                        }
+                        return res.data;
+                }
+            },
+            fail(p) {
+                Tip.errorToast('系统错误');
+                return false;
             }
-            return res.data;
-        }
-      },
-      fail(p) {
-        Tip.errorToast('系统错误');
-        return false;
-      }
-    });
-    this.intercept('uploadFile', {
-      config(p) {
-        p.name = 'file';
-        if (!p.formData) {
-          p.formData = {};
-        }
-        p.formData['token'] = G.token;
-        return p;
-      },
-      success(p) {
-        try {
-          p.data = JSON.parse(p.data);
-        } catch (e) {
-          Tip.errorToast('数据格式错误' + p.data);
-          return false;
-        }
-        if (p.data.ret == 200 && p.data.data.url) {
-          return p.data.data.url;
-        } else {
-          Tip.errorToast('上传失败' + p.data.msg);
-          return false;
-        }
-      },
-      fail(p) {
-        Tip.errorToast('系统错误');
-        return false;
-      }
-    });
-    wx.getSystemInfo({
-      success: function (res) {
-        wepy.G.systemInfo = res;
-      }
-    });
-    this.mounted(options)
-  }
-
-  getOpenId(callback = null) {
-    if (G.openId) {
-      if (callback) {
-        callback({});
-      }
-    } else {
-      wx.login({
-        success: async function (res) {
-          if (res.code) {
-            let result = await api.miniProgramLogin(res.code);
-            if (result) {
-              if (callback) {
-                callback(res);
-              }
+        });
+        this.intercept('uploadFile', {
+            config(p) {
+                p.name = 'file';
+                if (!p.formData) {
+                    p.formData = {};
+                }
+                p.formData['token'] = G.token;
+                return p;
+            },
+            success(p) {
+                try {
+                    p.data = JSON.parse(p.data);
+                } catch (e) {
+                    Tip.errorToast('数据格式错误' + p.data);
+                    return false;
+                }
+                if (p.data.ret == 200 && p.data.data.url) {
+                    return p.data.data.url;
+                } else {
+                    Tip.errorToast('上传失败' + p.data.msg);
+                    return false;
+                }
+            },
+            fail(p) {
+                Tip.errorToast('系统错误');
+                return false;
             }
-          } else {
-            Tip.errorToast('登录失败！' + res.errMsg);
-          }
-        }
-      });
+        });
+        wx.getSystemInfo({
+            success: function(res) {
+                wepy.G.systemInfo = res;
+            }
+        });
+        this.mounted(options)
     }
-  }
 
-  async doLogin(callback = null) {
-    if (G.token) {
-      Tip.hideLoading();
-      callback({})
-      return true
+    onError(msg) {
+        console.log(msg)
     }
-    let result = await api.autoLogin();
-    Tip.hideLoading();
-    if (result) {
-      if (callback) {
-        callback(result);
-      }
-    }
-  }
-
-  autoLogin(callback = null) {
-    Tip.showLoading();
-    let self = this
-    this.getOpenId(function (result) {
-      self.doLogin(callback)
-    })
-  }
-
-  onError(msg) {
-    console.log(msg)
-  }
 }
--- src/page.js
+++ src/page.js
@@ -1,31 +1,81 @@
 import wepy from 'wepy'
+import api from './api/auth';
+import Tip from 'tip';
 export default class page extends wepy.page {
-  mounted(options) {
 
-  }
-
-  render(options) {
-    this.setData('G', wepy.G)
-    this.G = wepy.G
-    this.setData('isMounted', true)
-    this.isMounted = true
-
-    this.mounted(options)
-  }
-
-  onLoad(options) {
-    if (this.config && this.config.requireTabbar) {
-      wx.showTabBar()
-    } else{
-      wx.hideTabBar()
+    getOpenId(callback = null) {
+        if (wepy.G.openId) {
+            if (callback) {
+                callback({});
+            }
+        } else {
+            wx.login({
+                success: async function(res) {
+                    if (res.code) {
+                        let result = await api.miniProgramLogin(res.code);
+                        if (result) {
+                            if (callback) {
+                                callback(res);
+                            }
+                        }
+                    } else {
+                        Tip.errorToast('登录失败！' + res.errMsg);
+                    }
+                }
+            });
+        }
     }
-    if (this.config && this.config.requireLogin) {
-      let self = this
-      this.$parent.autoLogin(function () {
-        self.render(options)
-      })
-    } else {
-      this.render(options)
+
+    async doLogin(callback = null) {
+        if (wepy.G.token) {
+            Tip.hideLoading();
+            callback({})
+            return true
+        }
+        let result = await api.autoLogin();
+        Tip.hideLoading();
+        if (result) {
+            if (callback) {
+                callback(result);
+            }
+        }
     }
-  }
+
+    autoLogin(callback = null) {
+        Tip.showLoading();
+        let self = this
+        this.getOpenId(function(result) {
+            self.doLogin(callback)
+        })
+    }
+
+
+    mounted(options) {
+
+    }
+
+    render(options) {
+        this.setData('G', wepy.G)
+        this.G = wepy.G
+        this.setData('isMounted', true)
+        this.isMounted = true
+
+        this.mounted(options)
+    }
+
+    onLoad(options) {
+        if (this.config && this.config.requireTabbar) {
+            wx.showTabBar()
+        } else {
+            wx.hideTabBar()
+        }
+        if (this.config && this.config.requireLogin) {
+            let self = this
+            this.$parent.autoLogin(function() {
+                self.render(options)
+            })
+        } else {
+            this.render(options)
+        }
+    }
 }
