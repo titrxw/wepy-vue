86da8cf2049b4b8807fa9c55489e73e706b4eaab
rxw <1120309488@qq.com>
2019-01-25 10:35:36 +0800

    Update tip.js

--- src/libs/tip.js
+++ src/libs/tip.js
@@ -1,50 +1,69 @@
 import wepy from 'wepy'
 export default {
-  errorToast(msg) {
-    wepy.showToast({
-      title: msg,
-      icon: 'none',
-      duration: 2000
+  errorToast(msg, duration = 2000) {
+    return new Promise(function (resolve, reject) {
+      wepy.showToast({
+        title: msg,
+        icon: 'none',
+        duration: duration
+      })
+      setTimeout(function () {
+        resolve(true)
+      }, duration)
     })
   },
 
-  successToast (msg) {
-    wepy.showToast({
-      title: msg,
-      icon: 'success',
-      duration: 2000
+  successToast(msg, duration = 2000) {
+    return new Promise(function (resolve, reject) {
+      wepy.showToast({
+        title: msg,
+        icon: 'success',
+        duration: duration
+      });
+      setTimeout(function () {
+        resolve(true)
+      }, duration)
     })
   },
 
-  toast (msg, icon = 'none', duration = 2000) {
-    wepy.showToast({
-      title: msg,
-      icon: icon,
-      duration: duration
+  toast(msg, icon = 'none', duration = 2000) {
+    return new Promise(function (resolve, reject) {
+      wepy.showToast({
+        title: msg,
+        icon: icon,
+        duration: duration
+      })
+      setTimeout(function () {
+        resolve(true)
+      }, duration)
     })
   },
 
-  showLoading (title = '加载中') {
+  showLoading(title = '加载中') {
     wepy.showLoading({
       title: title,
     })
   },
 
-  hideLoading () {
+  hideLoading() {
     wepy.hideLoading()
   },
 
-    /**
+  /**
    * 弹出确认窗口
    */
-  modal (text, title = '提示') {
+  modal(text, title = '提示') {
     return new Promise((resolve, reject) => {
       wepy.showModal({
         title: title,
         content: text,
         showCancel: false,
         success: res => {
-          resolve(false)
+          if (!res.cancel) {
+            resolve(true)
+          } else {
+            resolve(false)
+          }
         },
         fail: res => {
           resolve(false)
@@ -56,7 +75,7 @@ export default {
   /**
    * 弹出确认窗口
    */
-  confirm (text, title = '提示') {
+  confirm(text, title = '提示') {
     return new Promise((resolve, reject) => {
       wepy.showModal({
         title: title,
