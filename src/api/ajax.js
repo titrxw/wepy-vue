69b159cb4543cea169a4a611af747d05e1ca71a1
titrxw <1120309488@qq.com>
2019-03-04 21:30:20 +0800

    change

--- src/api/ajax.js
+++ src/api/ajax.js
@@ -2,27 +2,17 @@ import wepy from 'wepy';
 
 export default {
     post(url, data, header = {}) {
-        return new Promise((resolve, reject) => {
-            wepy.request({
-                url: url,
-                data: data,
-                method: 'POST',
-                header: header,
-                success: function(res) {
-                    resolve(res)
-                },
-                fail: function(error) {
-                    reject(error)
-                }
-            })
-        });
+        return this.request(url, data, 'POST', header)
     },
     get(url, data, header = {}) {
+        return this.request(url, data, 'GET', header)
+    },
+    request(url, data, method = 'GET', header = {}) {
         return new Promise((resolve, reject) => {
             wepy.request({
                 url: url,
                 data: data,
-                method: 'GET',
+                method: method,
                 header: header,
                 success: function(res) {
                     resolve(res)
