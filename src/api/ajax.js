39da640306be358bbcc385ce7608a3bfcb3ef5df
git <1120309488@qq.com>
2019-03-20 16:21:54 +0800

    change

--- src/api/ajax.js
+++ src/api/ajax.js
@@ -1,26 +1,40 @@
 import wepy from 'wepy';
 
+var throttle = {}
+
 export default {
-    post(url, data, header = {}) {
-        return this.request(url, data, 'POST', header)
-    },
-    get(url, data, header = {}) {
-        return this.request(url, data, 'GET', header)
-    },
-    request(url, data, method = 'GET', header = {}) {
-        return new Promise((resolve, reject) => {
-            wepy.request({
-                url: url,
-                data: data,
-                method: method,
-                header: header,
-                success: function(res) {
-                    resolve(res)
-                },
-                fail: function(error) {
-                    reject(error)
-                }
-            })
-        });
+  post(url, data, header = {}) {
+    return this.request(url, data, 'POST', header)
+  },
+  get(url, data, header = {}) {
+    return this.request(url, data, 'GET', header)
+  },
+  request(url, data,method = 'GET', header = {}) {
+    // 防止重复提交 暂时这样处理
+    let flag = JSON.stringify(arguments)
+    if (throttle[flag]) {
+      return false;
     }
+    throttle[flag] = true
+    return new Promise((resolve, reject) => {
+      wepy.request({
+        url: url,
+        data: data,
+        method: method,
+        header: header,
+        success: function (res) {
+          resolve(res)
+        },
+        fail: function (error) {
+          reject(error)
+        },
+        complete: function () {
+          let throttleTime = setTimeout(function () {
+            delete throttle[flag]
+            clearTimeout(throttleTime)
+          }, 1000)
+        }
+      })
+    });
+  }
 }
