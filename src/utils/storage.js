737e9f60f42f17960230a323764a5821a6b3cb5a
git <1120309488@qq.com>
2019-04-02 09:58:54 +0800

    change

---
+++ src/utils/storage.js
@@ -0,0 +1,56 @@
+var rememberData = {}
+export default {
+  set (key, data,expire = 0) {
+    if (!data) {
+      return false;
+    }
+    rememberData[key] = data
+    try{
+      wx.setStorageSync(key, data)
+      if (expire > 0) {
+        this.set(key +'-expire', {
+          cur: new Date().valueOf(),
+          expire: expire
+        })
+      }
+    } catch (e) {
+      console.log('storage error')
+    }
+  },
+  get (key, _default = null) {
+    let expire = null
+    try{
+      expire = wx.getStorageSync(key +'-expire')
+    } catch (e) {
+      expire = null
+    }
+    if (expire) {
+      let ctime = new Date().valueOf();
+      if (ctime - expire.cur > expire.expire) {
+        this.rm(key)
+        return null
+      }
+    }
+    
+    if (!rememberData[key]) {
+      let data = null
+      try{
+        data = wx.getStorageSync(key)
+      } catch (e) {
+        data = _default
+      }
+      rememberData[key] = data
+    }
+
+    return rememberData[key]
+  },
+  rm (key) {
+    delete rememberData[key]
+    try{
+      wx.removeStorageSync(key)
+      wx.removeStorageSync(key +'-expire')
+    } catch (e) {
+      console.log('storage error')
+    }
+  }
+}
