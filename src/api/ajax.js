362824263ca05e0ee83c19560dff5f7793e1929c
titrxw <1120309488@qq.com>
2019-04-03 22:11:08 +0800

    change

--- src/api/ajax.js
+++ src/api/ajax.js
@@ -1,40 +1,40 @@
 import wepy from 'wepy';
 
-let throttle = {}
+// let throttle = {}
 
 export default {
-  post(url, data, header = {}) {
-    return this.request(url, data, 'POST', header)
-  },
-  get(url, data, header = {}) {
-    return this.request(url, data, 'GET', header)
-  },
-  request(url, data,method = 'GET', header = {}) {
-    // 防止重复提交 暂时这样处理
-    let flag = url + '&' + method
-    if (throttle[flag]) {
-      return false;
+    post(url, data, header = {}) {
+        return this.request(url, data, 'POST', header)
+    },
+    get(url, data, header = {}) {
+        return this.request(url, data, 'GET', header)
+    },
+    request(url, data, method = 'GET', header = {}) {
+        // 防止重复提交 暂时这样处理
+        // let flag = url + '&' + method
+        // if (throttle[flag]) {
+        //   return false;
+        // }
+        // throttle[flag] = true
+        return new Promise((resolve, reject) => {
+            wepy.request({
+                url: url,
+                data: data,
+                method: method,
+                header: header,
+                success: function(res) {
+                    resolve(res)
+                },
+                fail: function(error) {
+                    reject(error)
+                },
+                // complete: function () {
+                //   let throttleTime = setTimeout(function () {
+                //     delete throttle[flag]
+                //     clearTimeout(throttleTime)
+                //   }, 1000)
+                // }
+            })
+        });
     }
-    throttle[flag] = true
-    return new Promise((resolve, reject) => {
-      wepy.request({
-        url: url,
-        data: data,
-        method: method,
-        header: header,
-        success: function (res) {
-          resolve(res)
-        },
-        fail: function (error) {
-          reject(error)
-        },
-        complete: function () {
-          let throttleTime = setTimeout(function () {
-            delete throttle[flag]
-            clearTimeout(throttleTime)
-          }, 1000)
-        }
-      })
-    });
-  }
 }
