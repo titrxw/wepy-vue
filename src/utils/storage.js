4bf14644088da6e26d0ac8dc350a688ac0bdae3e
titrxw <1120309488@qq.com>
2019-04-15 23:11:11 +0800

    change

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -1,59 +1,57 @@
 var rememberData = {}
 export default {
-  set (key, data,expire = 0) {
-    if (!data) {
-      return false;
-    }
-    rememberData[key] = data
-    try{
-      wx.setStorageSync(key, data)
-      if (expire > 0) {
-        this.set(key +'-expire', {
-          cur: new Date().valueOf(),
-          expire: expire
-        })
-      }
-    } catch (e) {
-      console.log('storage error')
-    }
-  },
-  get (key, _default = null) {
-    let expire = null
-    try{
-      expire = wx.getStorageSync(key +'-expire')
-    } catch (e) {
-      expire = null
-    }
-    if (expire) {
-      let ctime = new Date().valueOf();
-      if (ctime - expire.cur > expire.expire) {
-        this.rm(key)
-        return _default
-      }
-    }
-    
-    if (!rememberData[key]) {
-      let data = null
-      try{
-        data = wx.getStorageSync(key)
-      } catch (e) {
-        data = _default
-      }
-      if (!data) {
-        data = _default
-      }
-      rememberData[key] = data
-    }
+    set(key, data, expire = 0) {
+        if (!data) {
+            return false;
+        }
+        rememberData[key] = data
+        try {
+            wx.setStorageSync(key, data)
+            if (expire > 0) {
+                this.set(key + '-expire', new Date().valueOf() + expire)
+            }
+        } catch (e) {
+            console.log('storage error')
+        }
+    },
+    get(key, _default = null) {
+        let expire = null
+        try {
+            expire = wx.getStorageSync(key + '-expire')
+        } catch (e) {
+            expire = null
+        }
+        if (expire) {
+            if (ctime > expire) {
+                this.rm(key)
+                return _default
+            }
+        }
 
-    return rememberData[key]
-  },
-  rm (key) {
-    delete rememberData[key]
-    try{
-      wx.removeStorageSync(key)
-      wx.removeStorageSync(key +'-expire')
-    } catch (e) {
-      console.log('storage error')
+        if (!rememberData[key]) {
+            let data = null
+            try {
+                data = wx.getStorageSync(key)
+            } catch (e) {
+                data = _default
+            }
+            if (!data) {
+                data = _default
+            }
+            rememberData[key] = data
+        }
+
+        return rememberData[key]
+    },
+    rm(key) {
+        delete rememberData[key]
+        try {
+            wx.removeStorageSync(key)
+            if (rememberData[key + '-expire']) {
+                wx.removeStorageSync(key + '-expire')
+            }
+        } catch (e) {
+            console.log('storage error')
+        }
     }
-  }
 }
